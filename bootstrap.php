16ac0dfe31171c113d84e06659979e27c5ed5825
FloKnapp <office@florianknapp.de>
2025-12-05 23:26:34 +0100

    Preparations for v0.1.0 - Added polyfill for safePath guard, as standalone integration wouldn't work

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,7 @@ use NixPHP\Queue\Core\Queue;
 use NixPHP\Queue\Drivers\FileDriver;
 use function NixPHP\app;
 use function NixPHP\CLI\command;
+use function NixPHP\guard;
 
 command()->add(QueueWorkerCommand::class);
 command()->add(QueueRetryFailedCommand::class);
@@ -17,3 +18,19 @@ app()->container()->set(Queue::class, function() {
     $dPath = app()->getBasePath() . FileDriver::DEFAULT_DEADLETTER_PATH;
     return new Queue(new FileDriver($qPath, $dPath));
 });
+
+if (!guard()->has('safePath')) {
+    guard()->register('safePath', function ($path) {
+        if (
+            $path === '' ||
+            str_contains($path, '..') ||
+            str_starts_with($path, '/') ||
+            str_contains($path, '://') ||
+            !preg_match('/^[A-Za-z0-9_\/.-]+$/', $path)
+        ) {
+            throw new \InvalidArgumentException('Insecure path detected! Navigation outside of application root is not allowed.');
+        }
+
+        return $path;
+    });
+}
